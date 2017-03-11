1fd13d9059362535d07eb3914993b4141708163e
Loriyana <lsdoncheva@abv.bg>
2017-03-11 12:33:03 +0200

    initial commit

---
+++ Wanderlust/Clients/Wanderlust.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wanderlust.WebClient.MvcApplication" Language="C#" %>
---
+++ Wanderlust/Wanderlust.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Wanderlust.UnitTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
