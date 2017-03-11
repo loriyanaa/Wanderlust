10bae4fb76b291a361ff01973167d555b1857631
Loriyana <lsdoncheva@abv.bg>
2017-03-11 16:44:35 +0200

    fix

--- Wanderlust/Tests/Wanderlust.UnitTests/UnitTest1.cs
+++ Wanderlust/Tests/Wanderlust.UnitTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace Wanderlust.UnitTests
         [Test]
         public void Test1()
         {
-            Assert.IsTrue(false);
+            Assert.IsTrue(true);
         }
 
         [Test]
