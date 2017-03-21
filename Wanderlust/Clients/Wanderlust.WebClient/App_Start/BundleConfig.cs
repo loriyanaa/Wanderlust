f50f5e3552388ec86acb52880cb93362ed240381
Loriyana <lsdoncheva@abv.bg>
2017-03-21 17:37:49 +0200

    finished home page design

--- Wanderlust/Clients/Wanderlust.WebClient/App_Start/BundleConfig.cs
+++ Wanderlust/Clients/Wanderlust.WebClient/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace Wanderlust.WebClient
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/main.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
